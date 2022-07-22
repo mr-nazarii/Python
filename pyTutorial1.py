aeefed6edb32dcf948723120d9f467a83a047c53
Nazarii <nazariikubik@gmail.com>
2022-07-22 10:47:47 +0200

    pyTutorial1

---
+++ pyTutorial1.py
@@ -0,0 +1,19 @@
+# -------------------------1
+
+x = 2 + 2 + 2
+
+if (x == 4):
+
+    print("Hello there")
+else:
+    print("Generall Kenobi")
+
+
+# -------------------------2
+
+n = 0
+
+while n <= 5:
+    print("Almost there " + str(n))
+    n += 1
+print("Blast off")
